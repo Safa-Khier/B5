f90cf6193b6831cd7fb51c8ec2b282a64f5e333a
Safa Khier <safakhier@gmail.com>
2024-03-20 14:25:13 +0200

    last build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./i18n.js"; // Import the i18n configuration
+import "./i18n.js";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./AuthContext.js";
