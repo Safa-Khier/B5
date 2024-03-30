fc56cbbe1050ad731a61ce6bb39bf7cf263e903e
Safa Khier <safakhier@gmail.com>
2024-03-30 03:04:00 +0300

    cash In build

--- src/components/screens/home/dashboard/cashIn.jsx
+++ src/components/screens/home/dashboard/cashIn.jsx
@@ -1,17 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import Select from "react-select";
 import CreditCardForm from "../../../creditCard/creditCardForm";
+import { mcokCurrencies } from "../../../../../public/mockData.jsx";
+import { removeTrailingZeros } from "../../../../../public/publicFunctions.jsx";
+
 export default function CashIn() {
   const { t } = useTranslation();
 
   // State to track the active tab
   const [activeTab, setActiveTab] = useState("buy");
+  const [currencies, setCurrencied] = useState();
+  const [price, setPrice] = useState();
+  const [selectedCurrency, setSelectedCurrency] = useState();
+
+  useEffect(() => {
+    setCurrencied(
+      mcokCurrencies.map((currency) => ({
+        value: currency,
+        label: currency.name,
+        image: currency.image,
+        symbol: currency.symbol,
+      })),
+      setSelectedCurrency(mcokCurrencies[0].value),
+    );
+  }, []);
+
+  // Custom option component
+  const CustomOption = ({ innerProps, isFocused, isSelected, data }) => (
+    <div
+      {...innerProps}
+      className={`flex justify-between items-center p-2 ${isFocused && "bg-gray-300 dark:bg-gray-600"} ${isSelected && "font-bold text-custom-teal"}`}
+    >
+      <div className="flex justify-start h-[100%] items-center">
+        <img
+          className="w-6 h-6 mr-2"
+          loading="lazy"
+          src={data.image}
+          alt={data.label + " Logo"}
+        />
+        <div className="flex flex-col justify-center items-start">
+          {data.symbol.toUpperCase()}
+          <span className="text-xs text-gray-500 dark:text-gray-400">
+            {data.label}
+          </span>
+        </div>
+      </div>
+      {isSelected && <i className="material-icons">done</i>}
+    </div>
+  );
+
+  const CustomSingleValue = ({ data }) => (
+    <div className="flex items-center">
+      <img src={data.image} style={{ width: 20, height: 20, marginRight: 8 }} />
+      {"≈ " + currencyAmount() + " " + data.symbol.toUpperCase()}
+    </div>
+  );
+
+  const currencyAmount = () => {
+    if (!selectedCurrency || !price) return 0;
+    let amount = price / selectedCurrency.value.current_price;
+    return removeTrailingZeros(amount.toFixed(10));
+  };
 
   // Function to render the tab content based on the active tab
   const renderTabContent = () => {
     switch (activeTab) {
       case "buy":
-        return <CreditCardForm />;
+        return (
+          <div className="flex flex-col md:flex-row justify-center md:justify-around md:gap-20 items-center h-[600px] p-10 my-10 border rounded-xl">
+            <div className="w-[400px] h-full p-5 flex flex-col justify-between items-start border rounded-xl gap-5">
+              <div className="w-full h-full flex flex-col gap-10">
+                <div className="flex flex-col w-full">
+                  <label className="text-lg font-bold mb-1">Spend</label>
+                  <input
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    className="react-input w-full rounded focus:ring-transparent"
+                    placeholder="$10 - $2,000"
+                  />
+                </div>
+                <div className="flex flex-col w-full">
+                  <label className="text-lg font-bold">Receive</label>
+                  <Select
+                    value={selectedCurrency}
+                    className="react-select-container w-full  "
+                    classNamePrefix="react-select"
+                    placeholder={t("search") + "..."}
+                    onChange={(selectedOption) =>
+                      setSelectedCurrency(selectedOption)
+                    }
+                    components={{
+                      Option: CustomOption,
+                      SingleValue: CustomSingleValue,
+                    }}
+                    options={currencies}
+                  />
+                </div>
+              </div>
+
+              <button className="w-full bg-custom-teal p-3 rounded">Buy</button>
+            </div>
+            <CreditCardForm />
+          </div>
+        );
       case "sell":
         return (
           <div className="w-full h-40 p-5 border rounded-lg">
@@ -23,13 +115,32 @@ export default function CashIn() {
     }
   };
 
+  const renderTabButtonIcon = (tab) => {
+    switch (tab) {
+      case "buy":
+        return "shopping_cart";
+      case "sell":
+        return "sell";
+      case "history":
+        return "history";
+      default:
+        return "";
+    }
+  };
+
   // Function to render the tab content based on the active tab
   const renderTabButton = (tab, title) => {
     return (
       <div
         className={`flex flex-col justify-start items-center text-xl font-semibold ${activeTab === tab && "text-custom-teal"}`}
       >
-        <button onClick={() => setActiveTab(tab)}>{t(title)}</button>
+        <button
+          className="flex justify-center items-center gap-2 p-2 hover:text-gray-500 dark:hover:text-gray-300"
+          onClick={() => setActiveTab(tab)}
+        >
+          <i className="material-icons">{renderTabButtonIcon(tab)}</i>
+          {t(title)}
+        </button>
         {
           // Render the history icon for the active tab
           activeTab === tab && (
@@ -47,15 +158,12 @@ export default function CashIn() {
         <div className="flex w-full gap-5">
           {renderTabButton("buy", "Buy")}
           {renderTabButton("sell", "Sell")}
+          {renderTabButton("history", "History")}
         </div>
-        <button className="flex gap-2 p-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700">
-          <i className="material-icons">history</i>
-          History
-        </button>
       </div>
 
       {/* Tab content */}
-      <div className="m-5 h-full w-[70%]">{renderTabContent()}</div>
+      <div className="m-5 h-full">{renderTabContent()}</div>
     </div>
   );
 }
