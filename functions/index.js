89fcf40fbccc08c80e4d8754b652a25fa666c5c2
Safa Khier <safakhier@gmail.com>
2024-04-20 18:22:24 +0300

    fixed functions

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,6 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 const admin = require("firebase-admin");
@@ -116,26 +114,6 @@ exports.fetchAndStoreCryptoNews = functions.pubsub
       console.error("Error fetching crypto news data:", error);
     }
   });
-
-// Schedule this function to run every 24 hours to delete old news
-async function clearNewsData() {
-  const db = admin.firestore();
-  const cryptonewsRef = db.collection("cryptonews");
-
-  const snapshot = await cryptonewsRef.get();
-  if (snapshot.empty) {
-    console.log("No matching documents.");
-    return;
-  }
-
-  // Batch delete
-  const batch = db.batch();
-  snapshot.forEach((doc) => {
-    batch.delete(doc.ref);
-  });
-
-  await batch.commit(); // Commit the batch
-}
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
